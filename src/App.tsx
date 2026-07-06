19986120976a5bdd6e810e38a0b694afcd6a9721
Arshia <arshiaTheDev@gmail.com>
2026-07-06 13:31:38 +0330

    Add different page routes and their corresponding page components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, BrowserRouter } from "react-router";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Classes from "./pages/Classes";
+import Membership from "./pages/membership";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import About from "./pages/About";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="classes" element={<Classes />} />
+        <Route path="membership" element={<Membership />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
