0dd7b5736a5ff19062024f5a2cc6857bd01c344c
Arshia <arshiaTheDev@gmail.com>
2026-07-26 21:19:36 +0330

    Add the heading font to headings

--- src/features/Classes/ClassesLayout.tsx
+++ src/features/Classes/ClassesLayout.tsx
@@ -6,7 +6,7 @@ function ClassesLayout() {
     <>
       <article className="h-fit w-full bg-zinc-950 px-9 py-5 pt-10 text-white">
         <h3 className="mb-2 text-sm text-amber-400">Popular Classes</h3>
-        <h2 className="text-lg font-bold text-white">
+        <h2 className="font-heading text-lg font-bold tracking-wide text-white">
           Find a class you'll love
         </h2>
         <p className="text-xs text-stone-400">
--- src/features/Home/Carousel.tsx
+++ src/features/Home/Carousel.tsx
@@ -4,7 +4,7 @@ function Carousel() {
   return (
     <section className="w-full border-y-2 bg-black px-9 py-5 pb-10">
       <h3 className="mb-2 text-sm text-amber-400">Gym enviroment</h3>
-      <h2 className="text-lg font-bold text-white">
+      <h2 className="font-heading text-lg font-bold tracking-wide text-white">
         Check out our gym enviroment
       </h2>
       <ImageSlider />
